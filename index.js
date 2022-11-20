58fccc6367dcaf4b5fc21f78bdee4b38f4eb8986
graceywambui <grace.wambui@student.moringaschool.com>
2022-11-20 17:31:36 +0300

    removelast and first cat

--- index.js
+++ index.js
@@ -101,7 +101,29 @@
     //});
   //});
 
-  function removeLastCat(){
-    return cats.pop;
-  }
-  console.log(cats.pop);
+function removeLastCat(){
+  var lessCat=cats.slice(0,cats.length-1)
+  return lessCat;
+};
+ console.log(removeLastCat)
+  beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+  //describe('removeFirstCat()', function () {
+    //it('removes the first cat from the cats array and returns a new array, leaving the cats array unchanged', function () {
+      //expect(removeFirstCat()).to.have.ordered.members(["Otis", "Garfield"]);
+
+      //expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
+    //});
+  //});
+//});
+//});
+function removeFirstCat(){
+  var lesscats=cats.slice(1)
+  return lesscats;
+};
+
+console.log(removeFirstCat("placeholder"))
